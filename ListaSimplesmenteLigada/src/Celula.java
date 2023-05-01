77d1be3c08757284181e7edcca4af57d84aefef7
Felipe <felipefggw@gmail.com>
2023-05-01 19:48:27 -0300

    Subindo Primeiras classes

--- ListaSimplesmenteLigada/src/Celula.java
+++ ListaSimplesmenteLigada/src/Celula.java
@@ -1,4 +1,21 @@
 
 public class Celula {
-
+	private String valor; //sera o valor armazenado dentro da celula da lista 
+	private Celula proximo; //sera utilizado para apontar para a proxima celula da lista 
+	
+	public String getValor() {
+		return this.valor;
+	}
+	
+	public void setValor(String valor) {
+		this.valor = valor;
+	}
+	
+	public Celula getCelula() {
+		return this.proximo;
+	}
+	
+	public void setValor(Celula proximo) {
+		this.proximo = proximo;
+	}
 }
---
+++ ListaSimplesmenteLigada/src/Lista.java
@@ -0,0 +1,34 @@
+
+public interface Lista {
+	//Tem inicio 
+	//Tem um fim
+	//Tem um tamanho 
+	//E possivel adicionar elementos
+	//E possivel adicionar elementos em posicoes especificas
+	//E possivel remover elementos
+	//E possivel remover elementos de acordo com a posicao
+	//E possivel pegar um elemento de acordo com a posicao
+	//E possivel pegar a posicao de acordo com o elemento
+	//E possivel saber se a lista esta vazia
+	//E possivel esvaziar a lista
+	
+	public int getTamanho(int valor); //retorna o tamanho da lista 
+	
+	public void getAdicionarElemento(); //adicionar um elemento ao final da lista
+	
+	public void getAdicionarElementoPosicao(String valor, int posicao); //adiciona um elemento em uma posicao especifica
+	
+	public void getRemoverElemento(); //remove um elemento da lista 
+
+	public void getRemoverElementoPosicao(String valor, int posicao); // remove um elemeto da lista de acordo com sua posicao
+
+	public String getElemento(int Posicao); //retorna o elemeto da lista de acordo com a sua posicao
+	
+	public int getPosicao(String valor); //retorna a posicao do elemento, e deve retornar -1 se ele nao existir na lista 
+	
+	public boolean isempty(); //verifica se a lista esta vazia 
+	
+	//public boolean estaVazia()//verifica se a lista esta vazia 
+	
+	public void getLimparLista(); //esvazia a lista
+}
